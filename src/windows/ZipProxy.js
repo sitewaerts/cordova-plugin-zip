19d85ef6c551d0ecf251004c6b517b40b350eac2
schauerte <schauerte@sitewaerts.de>
2020-06-13 17:49:04 +0200

    bugfixes an path handling

--- src/windows/ZipProxy.js
+++ src/windows/ZipProxy.js
@@ -3,9 +3,11 @@ var JSZip = require("./JsZip");
 var storage = Windows.Storage; // Alias for readability
 
 
-function getFileAsUint8Array(file) {
+function getFileAsUint8Array(file)
+{
     return storage.FileIO.readBufferAsync(file)
-        .then(function (buffer) {
+        .then(function (buffer)
+        {
             //Read the file into a byte array
             var fileContents = new Uint8Array(buffer.length);
             var dataReader = storage.Streams.DataReader.fromBuffer(buffer);
@@ -16,43 +18,102 @@ function getFileAsUint8Array(file) {
         });
 }
 
-function unzip(filename, outputDir) {
+function resolveOutDir(outputDir)
+{
+    return new WinJS.Promise(function (resolve, reject)
+    {
+
+        function _error(error)
+        {
+            error = {
+                message: "cannot resolve",
+                dir: outputDir,
+                cause: error
+            };
+            console.error(error);
+            reject(error);
+        }
+
+        function _success(result)
+        {
+            console.log("successfully resolved " + outputDir, result);
+            resolve(result);
+        }
+
+        window.resolveLocalFileSystemURL(outputDir,
+            function (dir)
+            {
+                console.log(dir);
+
+                var targetFolder;
+
+                // cordova.file.cacheDirectory and cordova.file.tempDirectory overlap on windows ....
+                // if (dir.nativeURL.indexOf(cordova.file.cacheDirectory) === 0)
+                //     targetFolder = storage.ApplicationData.current.localCacheFolder;
+                if (dir.nativeURL.indexOf(cordova.file.tempDirectory) === 0)
+                    targetFolder = storage.ApplicationData.current.temporaryFolder;
+                else if (dir.nativeURL.indexOf(cordova.file.syncedDataDirectory) === 0)
+                    targetFolder = storage.ApplicationData.current.roamingFolder;
+                else if (dir.nativeURL.indexOf(cordova.file.dataDirectory) === 0)
+                    targetFolder = storage.ApplicationData.current.localFolder;
+                else
+                {
+                    _error("cannot map native url to file system: " + dir.nativeURL);
+                    return;
+                }
+
+                targetFolder.getFolderAsync(dir.fullPath.substring(1).replace(/\//g, '\\')).then(_success, _error);
+            },
+            _error
+        );
+
+    });
+
+
+}
+
+function unzip(filename, outputDir)
+{
     var fileCollisionOption = storage.CreationCollisionOption.replaceExisting;
 
     return storage.StorageFile
-        .getFileFromPathAsync(filename)
+        .getFileFromApplicationUriAsync(new Windows.Foundation.Uri(filename))
         .then(getFileAsUint8Array)
-        .then(function (zipFileContents) {
-            //Create the zip data in memory
-            var zip = new JSZip(zipFileContents);
+        .then(function (zipFileContents)
+        {
+            return resolveOutDir(outputDir)
+                .then(function (outFolder)
+                {
+                    //Create the zip data in memory
+                    var zip = new JSZip(zipFileContents);
 
-            //Extract files
-            var promises = [];
-            var lf = storage.ApplicationData.current.localFolder;
-            _.each(zip.files, function (zippedFile) {
+                    //Extract files
+                    return WinJS.Promise.join(Object.values(zip.files).map(function (zippedFile)
+                    {
 
-                //Create new file
-                promises.push(lf
-                    .createFileAsync(zippedFile.name, fileCollisionOption)
-                    .then(function (localStorageFile) {
-                        //Copy the zipped file's contents into the local storage file
-                        var fileContents = zip.file(zippedFile.name).asUint8Array();
-                        return storage.FileIO
-                            .writeBytesAsync(localStorageFile, fileContents);
-                    })
-                );
-            });
-
-            return WinJS.Promise.join(promises);
+                        //Create new file
+                        return outFolder.createFileAsync(zippedFile.name, fileCollisionOption)
+                            .then(function (localStorageFile)
+                            {
+                                //Copy the zipped file's contents into the local storage file
+                                var fileContents = zip.file(zippedFile.name).asUint8Array();
+                                return storage.FileIO
+                                    .writeBytesAsync(localStorageFile, fileContents);
+                            });
+                    }));
+                });
         });
 }
 
-cordova.commandProxy.add("Zip",{
-    unzip:function(successCallback,errorCallback, args) {
-        if(!args || !args.length) {
+cordova.commandProxy.add("Zip", {
+    unzip: function (successCallback, errorCallback, args)
+    {
+        if (!args || !args.length)
+        {
             errorCallback("Error, something was wrong with the input filename. =>" + filename);
         }
-        else {
+        else
+        {
             unzip(args[0], args[1]).done(successCallback());
 
         }
