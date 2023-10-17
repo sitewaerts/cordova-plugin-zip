699a323bf0c8bf799a1b05a0e2a8831a461bfa4d
Chris <chris@lunetic.de>
2023-10-17 19:34:19 +0200

    3.2.8 - Fixed electron implementation not returning a promise - Removed unused src/jszip.min.js reference for electron implementation - Added note that progressCallback currently isn't working - Disabled logging to console

--- src/electron/ZipProxy.js
+++ src/electron/ZipProxy.js
@@ -1,57 +1,4 @@
-// package: decompress
-// > npm install decompress --save
-// NOTE: Has no progress indication
-const decompress = require("decompress");
-
-// package: extract-zip
-// > npm install extract-zip --save
-//const extract = require('extract-zip');
-
-/**
- * @param {string} filename
- * @param {string} outputDir
- * @param {function(total:number, loaded:number):void} progressCallback
- * @return {PromiseLike<any>}
- */
-async function unzipNode(filename, outputDir, progressCallback) {
-    
-
-    // Implementation for package: decompress
-    /*
-    const unzipPromise = await new Promise((resolve, reject) => {
-        try {
-            decompress(filename, outputDir)
-                .then(files => { resolve(); })
-                .catch(error => { reject(error); });
-        } catch (error) {
-            reject(error);
-        }
-    });
-    */
-    
-    // Implementation for package: extract-zip
-    /*
-    const unzipPromise = await new Promise(async (resolve, reject) => {
-        try {
-            await extract(filename, {
-                dir: outputDir,
-                onEntry: (entry, zipfile) => {
-                    if (progressCallback) {
-                        progressCallback({ loaded: zipfile.entriesRead, total: zipfile.entryCount });
-                    }
-                }
-            });
-            resolve();
-        } catch (error) {
-            reject(error);
-        }
-    });
-    */
-
-    return await unzipPromise;
-}
-
-const logEnabled = true;
+const logEnabled = false;
 
 function log(message) {
     if (logEnabled !== true) {
@@ -60,30 +7,74 @@ function log(message) {
     console.log(`cordova-plugin-zip > src > electron > ZipProxy.js: ${message}`);
 }
 
+
+/**
+ * Implementation using package: decompress
+ * > npm install decompress --save
+ * NOTE: Doesn't support progress callbacks so we don't use this implementation but keep it as a reference for testing.
+ * 
+ * @param {string} filename
+ * @param {string} outputDir
+ * @param {function(total:number, loaded:number):void} progressCallback
+ * @return {PromiseLike<any>}
+ */
+/*
+const decompress = require("decompress");
+async function unzipWithDecompress(filename, outputDir, progressCallback) {
+    log(`unzipWithDecompress = ${filename} -> ${outputDir}, progressCallback = ${progressCallback}`);
+    return decompress(filename, outputDir)
+        .then(files => { resolve(); })
+        .catch(error => { reject(error); });
+}
+*/
+
+
+/**
+ * Implementation using package: extract-zip
+ * > npm install extract-zip --save
+ * 
+ * @param {string} filename
+ * @param {string} outputDir
+ * @param {function(total:number, loaded:number):void} progressCallback
+ * @return {PromiseLike<any>}
+ */
+const extract = require('extract-zip');
+async function unzipWithExtractZip(filename, outputDir, progressCallback) {
+    log(`unzipWithExtractZip = ${filename} -> ${outputDir}, progressCallback = ${progressCallback}`);
+    return extract(filename, {
+        dir: outputDir,
+        onEntry: (entry, zipfile) => {
+            log(`unzipWithExtractZip: entry = ${entry.fileName}`);
+            if (progressCallback) {
+                progressCallback({ loaded: zipfile.entriesRead, total: zipfile.entryCount });
+            }
+        }
+    });
+}
+
+
 module.exports = {
-    unzip: function(args)
+    /**
+     * @param {[fileName: String, outputDirectory: String]} args
+     *      fileName - the full path of the zip archive
+     *      outputDirectory - the full path of the directory where to extract the zip contents
+     * @returns {Promise<void>} resolves when everything has been extracted successfully
+     */
+    unzip: function([args])
     {
-        // Why are the arguments packed like this (array inside array)?
-        // And how can we access the success/error/progress callbacks passed into cordova.exec() in '../../zip.js' ?
-        const fileName = args[0][0];
-        const outputDirectory = args[0][1];
+        const fileName = args[0];
+        const outputDirectory = args[1];
+        const progressCallback = ({ total, loaded }) => {
+            // TODO: Dummy progress callback until we have a solution for the issue that the electron implementation
+            // doesn't offer a way to use them.
+            log(`unzip: progressCallback(total: ${total}, loaded: ${loaded})`);
+        };
 
         log(`unzip: args = ${JSON.stringify(args)}`);
         log(`unzip: fileName = ${fileName}`);
         log(`unzip: outputDirectory = ${outputDirectory}`);
         
-        try {
-            decompress(fileName, outputDirectory)
-                .then(files => { log(`unzip: success`); })
-                .catch(error => { log(`unzip: error = ${error}`); });
-            
-            // TODO: Call success callback
-            return true;
-        } catch (error) {
-            // TODO: Call error callback
-            log(`unzip: catch(error) = ${error}`);
-            return false;
-        }
+        return unzipWithExtractZip(fileName, outputDirectory, progressCallback)
     }
 };
 
